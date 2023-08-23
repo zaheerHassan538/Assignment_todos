18f4b20b9ade56de0d6c152f5c6f648bad986736
zaheerHassan538 <hassan.zaheer@emumba.com>
2023-08-23 12:48:23 +0000

    crud for user

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,7 +1,36 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"mytodoapp/database"
+	"mytodoapp/models"
 
-func UserController(c *gin.Context) {
-	c.String(200, "Hello World3!")
+	"github.com/gin-gonic/gin"
+)
+
+func GetUsers(c *gin.Context) {
+	users := []models.User{}
+	database.DB.Find(&users)
+	c.JSON(200, &users)
+}
+
+func CreateUser(c *gin.Context) {
+	var user models.User
+	c.BindJSON(&user)
+	database.DB.Create(&user)
+	c.JSON(200, &user)
+}
+
+func DeleteUser(c *gin.Context) {
+	var user models.User
+	database.DB.Where("id= ?", c.Param("id")).Delete(&user)
+	c.JSON(200, &user)
+}
+
+func UpdateUser(c *gin.Context) {
+	var user models.User
+	database.DB.Where("id=?", c.Param("id")).First(&user)
+	c.BindJSON(&user)
+	database.DB.Save(&user)
+	c.JSON(200, &user)
+
 }
--- models/models.go
+++ models/models.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Id       int    `json:"id" gorm:"primary_key"`
+	Id       int    `json:"ID" gorm:"primary_key"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
